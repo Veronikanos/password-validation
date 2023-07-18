e58e68f5d83b1195a1243c1e6192530f760d3560
Veronica <vveronikanika@gmail.com>
2023-07-18 22:35:47 +0300

    feat: add formControl with custom validators

--- src/app/password/form/form.component.ts
+++ src/app/password/form/form.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  Validators,
+  ValidatorFn,
+} from '@angular/forms';
+
+// Custom validator functions
+function customValidator1(): ValidatorFn {
+  return (control: AbstractControl) => {
+
+    console.log(control);
+    const valid = control.value.includes('5');
+    return valid ? { first: true } : null;
+  };
+}
+
+function customValidator2(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const valid = control.value.includes('2');
+    return valid ? { customValidation2: true } : null;
+  };
+}
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  templateUrl: 'form.component.html',
+  styleUrls: ['form.component.css'],
 })
 export class FormComponent {
-
+  passwordFormControl = new FormControl('', [
+    customValidator1(),
+    customValidator2(),
+  ]);
 }
--- src/app/password/password.module.ts
+++ src/app/password/password.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FormComponent } from './form/form.component';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { NgIf } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    FormComponent
-  ],
+  declarations: [FormComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    NgIf,
+  ],
+  exports: [FormComponent],
 })
-export class PasswordModule { }
+export class PasswordModule {}
