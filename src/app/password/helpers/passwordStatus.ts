4a67328604341dee14953b0db95224d46c1eee67
Veronica <vveronikanika@gmail.com>
2023-07-20 14:23:07 +0300

    Auto-generated commit [ci skip]

--- src/app/password/helpers/passwordStatus.ts
+++
@@ -1,7 +0,0 @@
-export const PasswordStatus = {
-  Empty: ['gray', 'gray', 'gray'],
-  TooShort: ['red', 'red', 'red'],
-  Easy: ['red', 'gray', 'gray'],
-  Medium: ['yellow', 'yellow', 'gray'],
-  Strong: ['green', 'green', 'green'],
-}
